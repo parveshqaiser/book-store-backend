beeaadd2ed9e3aa25c86c75e237a54c8a6c00475
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-08-13 13:16:13 +0530

    added frontend url

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(parser());
 // }));
 
 app.use(cors({
-    origin: ["http://localhost:5173","http://localhost:5174"],
+    origin: ["http://localhost:5173","http://localhost:5174","https://bookstore-frontend-xja0.onrender.com"],
     methods : ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
